fcef5a3364d40e1d22c34f9d5647eb0ee33be74a
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 23:11:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,8 +13,9 @@ end
 
 def current_player(board)
   if turn_count(board) % 2 == 0
-    current_player = X
+    current_player_turn = X
   else
-    current_player = O
+    current_player_turn = O
   end
+  current_player_turn
 end
