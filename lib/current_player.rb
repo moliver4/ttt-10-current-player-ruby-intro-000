f3d82d80d20c0c9a83e1b246167da904d44f5208
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 22:46:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 count = 0
 def turn_count(board)
   board.each do |turns|
-    if board(turns) == "X" || board(turns) == "O"
-    count += 1
-    end
+  if board(turns) == "X" || board(turns) == "O"
+  count += 1
   end
 end
